5d2e53c8face91714e0a150b5198849847b373a9
Pablo Barrenechea <pbarrenechea@devspark.com>
2016-12-02 11:05:56 -0300

    resized image when it's been uploaded

--- www/lib/ion-profile-picture/ion-profile-picture.js
+++ www/lib/ion-profile-picture/ion-profile-picture.js
@@ -10,17 +10,15 @@ angular.module('ion-profile-picture', [])
       return {
         require: '?ngModel',
         restrict: 'E',
-        template: '<div class="ion-profile-picture no-picture"><input type="file" accept="image/*" capture /></div>',
+        template: '<div class="ion-profile-picture no-picture"><input type="file" accept="image/*" capture /> <canvas class="ion-profile-picture-canvas" id="tempCanvas"></canvas> </div>',
         replace: true,
         link: function(scope, element, attrs, ngModel) {
           var $input = angular.element(element.find('input'));
           var file = undefined;
-
           if(!ngModel){
             console.error('ion-profile-picture:', 'Need to set ng-model');
             return false;
           }
-
           // all this guy does is trigger a click event on the hidden file input
           var openFileDialog = function(e){
             $input[0].click();
@@ -29,10 +27,34 @@ angular.module('ion-profile-picture', [])
           // every time the file gets updated, this guy does it's thing
           var onFilePick = function(e){
             var reader = new FileReader();
-
             reader.onload = function(_e){
               scope.$apply(function(){
-                ngModel.$setViewValue(_e.target.result);
+                var img = document.createElement("img");
+                img.src = _e.target.result;
+                var canvas = document.getElementById('tempCanvas');
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0);
+                var MAX_WIDTH = 200;
+                var MAX_HEIGHT = 200;
+                var width = img.width;
+                var height = img.height;
+                if (width > height) {
+                  if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                  }
+                } else {
+                  if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                  }
+                }
+                canvas.width = width;
+                canvas.height = height;
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0, width, height);
+                var dataurl = canvas.toDataURL("image/png");
+                ngModel.$setViewValue(dataurl);
                 ngModel.$render();
               })
             };
