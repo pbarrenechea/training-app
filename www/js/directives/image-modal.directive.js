f99f12598792c3abd556999a0cb079b1042a88d7
Pablo Barrenechea <pbarrenechea@devspark.com>
2016-12-07 11:22:44 -0300

    Added user avatar

---
+++ www/js/directives/image-modal.directive.js
@@ -0,0 +1,51 @@
+
+(function() {
+  'use strict';
+  angular
+    .module('Training')
+    .directive('imageModal', function () {
+      return {
+      restrict: 'E',
+        templateUrl: 'views/image-modal.directive.html',
+        scope: {
+        'source': '=source'
+      },
+      controller: function($scope, $ionicModal){
+        $ionicModal.fromTemplateUrl('image-modal.html', {
+          scope: $scope,
+          animation: 'slide-in-up'
+        }).then(function(modal) {
+          $scope.modal = modal;
+        });
+
+        $scope.openModal = function() {
+          $scope.modal.show();
+        };
+
+        $scope.closeModal = function() {
+          $scope.modal.hide();
+        };
+
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function() {
+          $scope.modal.remove();
+        });
+        // Execute action on hide modal
+        $scope.$on('modal.hide', function() {
+          // Execute action
+        });
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function() {
+          // Execute action
+        });
+        $scope.$on('modal.shown', function() {
+          console.log('Modal is shown!');
+        });
+
+        $scope.showImage = function() {
+          $scope.openModal();
+        }
+      }
+    }
+  });
+})();
