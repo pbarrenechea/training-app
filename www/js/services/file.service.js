129d080b6042bb1040d710d03128e53e7d273807
Pablo Barrenechea <pbarrenechea@devspark.com>
2016-12-30 09:35:21 -0300

    finished card-28

--- www/js/services/file.service.js
+++ www/js/services/file.service.js
@@ -19,12 +19,20 @@
 
     function createFile(fileName, content){
      return  $cordovaFile.createFile(cordova.file.externalDataDirectory, fileName, true).then(function(file){
-        $log.info("File created!!!");
-       file.createWriter(function(fileWriter) {
-         fileWriter.seek(fileWriter.length);
-         var blob = new Blob([content], {type:'text/plain'});
-         fileWriter.write(blob);
-         $log.info("ok, in theory i worked");
+       $log.info("File created!!!");
+       file.createWriter(function(fileWriter) {//Attaching callback, this will execute after the truncate below
+         fileWriter.onwriteend = function(evt) {//After truncating, file gets overridden
+           $log.info("Startig writing");
+           fileWriter.seek(0);
+           var blob = new Blob([content], {type:'text/plain'});
+           fileWriter.onwriteend = function(evt){
+             $log.info("Finished writing " + fileName);
+           };
+           fileWriter.write(blob);
+
+           $log.info("File modified succesfully");
+         };
+         fileWriter.truncate(0);
        }, function(err){
          $log.error(err);
        });
